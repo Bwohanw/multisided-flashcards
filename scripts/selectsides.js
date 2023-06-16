9e1d9597b732f4b11709155936866abbd401015f
Bwohan Wang <bwohanw2@illinois.edu>
2023-06-16 11:42:23 -0400

    the getting of the order of cards while testing working

--- scripts/selectsides.js
+++ scripts/selectsides.js
@@ -7,17 +7,17 @@ var thesecards = existing[localStorage.getItem("newname")];//the list of flashca
 var topiclist = Object.keys(thesecards[0]);//a list of all the topics for the corresponding set
 const totalsides = localStorage.getItem("numsides");//the total number of topics you want to be tested on
 
-var counter = 0; //number of remaining topics to choose
+var remainder = totalsides; //number of remaining topics to choose
 
-const topicorder = []; //array of integers representing the index order of the topics
+var topicorder = []; //array of the topics in order
 const tombstoning = []; //array of 1 and -1 used for tombstoning the topics, if it's a -1 the topic has been chosen before, 1 otherwise.
-//currently not used because i'm removing from the selector instead of adding to it
+//currently not used because i'm removing from the selector instead of adding to it, and it might mess with indexing
 for (var i = 0; i < topiclist.length; i++) {
     tombstoning.push(1);
 }
 
 document.querySelector(".formbox h1").innerHTML = "Pick the first side you would like to be tested on:";
-document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + totalsides-counter + " left to choose";
+document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + remainder + " left to choose";
 
 function addOption(element) {
     var option = document.createElement("option");
@@ -28,11 +28,10 @@ function addOption(element) {
     selector.appendChild(option);
 }
 
+
 function addAvailableOptions() {
     for (var i = 0; i < topiclist.length; i++) {
-        if (tombstoning[i] === 1) {
-            addOption(topiclist[i]);
-        }
+        addOption(topiclist[i]);
     }
 }
 
@@ -44,22 +43,38 @@ infoform.addEventListener("submit", (e) => {
     e.preventDefault();
     var x = document.getElementById("flashcardlist");
     var idx = x.selectedIndex;
-    topicorder.push(idx);
-    tombstoning[idx] = -1;
-    counter++;
+    topicorder.push(topiclist[idx]);
+
+    var temp = [];
+    for (var i = 0; i < topiclist.length; i++) {
+        if (i != idx) {
+            temp.push(topiclist[i]);
+        }
+    }
+
+    topiclist = temp;
+    remainder--;
+
     x.remove(idx);
+    console.log(topicorder);
 
     document.querySelector(".formbox h1").innerHTML = "Pick the next side you would like to be tested on:";
-    document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + totalsides-counter + " left to choose";
+    document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + remainder + " left to choose";
 
     var x = document.createElement("OL");
     x.setAttribute("id", "sideorder");
     document.body.appendChild(x);
 
     var y = document.createElement("LI");
-    var t = document.createTextNode(topiclist[idx]);
+    var t = document.createTextNode(topicorder[topicorder.length - 1]);
     y.appendChild(t);
     document.getElementById("sideorder").appendChild(y);
+
+    if (remainder === 0) {
+        var storage = topicorder.join(';');
+        localStorage.setItem("flashcardorder", storage);
+        window.location.href = "testflashcards.html";
+    }
 })
 
 
@@ -67,14 +82,15 @@ document.getElementById("undo").addEventListener("click", (e) => {
     e.preventDefault();
     if (topicorder.length === 0) {
         document.querySelector(".formbox h1").innerHTML = "Pick the first side you would like to be tested on:";
-        document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + (totalsides-counter) + " left to choose";
+        document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + remainder + " left to choose";
         return;
     }
-    counter--;
-    if (counter === 1) {
+    remainder++;
+    if (remainder==totalsides) {
         document.querySelector(".formbox h1").innerHTML = "Pick the first side you would like to be tested on:";
-        document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + (totalsides-counter) + " left to choose";
+        document.querySelector(".formbox p").innerHTML = "Total sides: " + totalsides + ", " + remainder + " left to choose";
     }
+    if (remainder > totalsides) remainder = totalsides;
 
     var list=document.getElementById("sideorder");
     var listItems = list.getElementsByTagName("li");
@@ -82,6 +98,6 @@ document.getElementById("undo").addEventListener("click", (e) => {
     list.removeChild(last);
 
     var latest = topicorder.pop();
-    tombstoning[latest] = 1;
-    addOption(topiclist[latest]);
+    topiclist.push(latest)
+    addOption(latest);
 })
