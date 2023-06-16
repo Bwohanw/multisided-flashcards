658b3c03087be922373601de4b9e92296865bd5b
Bwohan Wang <bwohanw2@illinois.edu>
2023-06-16 07:51:38 -0400

    created dropdown of all the existing flashcards when the click use existing, and implemented the edit deck option

---
+++ scripts/selectset.js
@@ -0,0 +1,39 @@
+existing = {}
+temp = localStorage.getItem("flashcardset");
+if (temp != null) {
+    existing = JSON.parse(temp);
+}
+
+const keylist = Object.keys(existing);
+
+
+function addOption(element) {
+    var option = document.createElement("option");
+    option.setAttribute("value", element);
+    var textNode = document.createTextNode(element);
+    option.appendChild(textNode);
+    var selector = document.getElementById("flashcardlist");
+    selector.appendChild(option);
+}
+
+
+for (var i = 0; i < keylist.length; i++) {
+    addOption(keylist[i]);
+}
+
+let infoform = document.getElementById("formcontent");
+infoform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var setname = document.getElementById("flashcardlist").value;
+    if (setname.length === 0) {
+        alert("Create a flashcard deck first!");
+        return;
+    }
+    localStorage.setItem("newname", setname);
+    var cardlist = existing[setname];
+    var topiclist = Object.keys(cardlist[0]);
+    let topics = topiclist.join(';');
+    alert(topics);
+    localStorage.setItem("newtopics", topics);
+    window.location.href="editortest.html";
+})
