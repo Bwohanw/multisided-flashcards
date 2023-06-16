f53f9b6242999ae60f80e5313b54d7a45d26d31e
Bwohan Wang <bwohanw2@illinois.edu>
2023-06-16 12:29:28 -0400

    finished testing flashcards

---
+++ scripts/testflashcards.js
@@ -0,0 +1,85 @@
+var newname = localStorage.getItem("newname");
+
+existing = {}
+temp = localStorage.getItem("flashcardset");
+if (temp != null) {
+    existing = JSON.parse(temp);
+}
+
+var flashcards = existing[newname];//the list of flashcards in order
+var shuffledindexes = []//list of indices of the flashcards list that will make shuffling easier
+for (var i = 0; i < flashcards.length; i++) {
+    shuffledindexes.push(i);
+}
+
+const topics = localStorage.getItem("flashcardorder");//list of the topics actually being studied
+var topicsarr = topics.split(';');
+
+var whichtopic = 0;
+var whichcard = 0;
+
+const numTopics = topicsarr.length;
+
+function updateinfo() {
+    document.querySelector(".topic h3").innerHTML = topicsarr[whichtopic] + ':';
+    document.querySelector(".topic p").innerHTML = 'card ' + (whichcard + 1) + '/' + flashcards.length + ", side " + (whichtopic+1)  + '/' + topicsarr.length;
+    document.querySelector(".notes h1").innerHTML = flashcards[shuffledindexes[whichcard]][topicsarr[whichtopic]];
+}
+
+function shuffle(array) {
+    let cindex = array.length, randomIndex;
+    while (cindex != 0) {
+        randomIndex = Math.floor(Math.random() * cindex);
+        cindex--;
+        var temp = array[cindex];
+        array[cindex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+    return array;
+}
+
+shuffledindexes = shuffle(shuffledindexes);
+
+document.querySelector(".notes h1").innerHTML = flashcards[shuffledindexes[whichcard]][topicsarr[whichtopic]];
+
+document.querySelector(".topic h2").innerHTML = "Flashcard Set: " + newname;
+document.querySelector(".topic h3").innerHTML = topicsarr[whichtopic] + ':';
+document.querySelector(".topic p").innerHTML = 'card ' + (whichcard + 1) + '/' + flashcards.length + ", side " + (whichtopic+1)  + '/' + topicsarr.length;
+
+
+document.getElementById("flipside").addEventListener('click', () => {
+
+
+    whichtopic = (whichtopic + 1) % numTopics;
+    updateinfo();
+
+});
+
+document.getElementById("langle").addEventListener('click', () => {
+
+    whichtopic = 0;
+    whichcard = whichcard-1 < 0 ? flashcards.length - 1 : whichcard-1;
+    updateinfo();
+
+    console.log(flashcards);
+})
+
+document.getElementById("rangle").addEventListener('click', () => {
+
+    whichtopic = 0;
+    whichcard = (whichcard + 1) % flashcards.length;
+    updateinfo();
+
+    console.log(flashcards);
+})
+
+document.getElementById("finish").addEventListener("click", () => {
+    window.location.href = "index.html";
+})
+
+document.getElementById("shuffle").addEventListener("click", () => {
+    shuffledindexes = shuffle(shuffledindexes);
+    whichtopic = 0;
+    whichcard = 0;
+    updateinfo();
+})
--- scripts/testorder.js
+++ scripts/testorder.js
@@ -8,12 +8,15 @@ var topiclist = Object.keys(thesecards[0]);//a list of all the topics for the co
 
 
 
+document.querySelector(".formbox h2").innerHTML = "Selected Deck: " + localStorage.getItem("newname");
+document.querySelector(".formbox p").innerHTML = "Maximum: " + topiclist.length + " sides";
+
 let infoform = document.getElementById("formcontent");
 infoform.addEventListener("submit", (e) => {
     e.preventDefault();
     var number = document.getElementById("howmany").value;
     if (number > topiclist.length) {
-        alert("Please enter the number of sides you would like to be tested on");
+        alert("Too many sides, try a smaller number.");
         return;
     }
     localStorage.setItem("numsides", number);
